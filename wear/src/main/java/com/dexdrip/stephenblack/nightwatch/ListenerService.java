4d28adac9bb2b1bef4b1c231d83206c7d6fc1633
AdrianLxM <adrian.m@inbox.com>
2015-06-12 15:35:26 +0200

    open settings remotely from phone

--- wear/src/main/java/com/dexdrip/stephenblack/nightwatch/ListenerService.java
+++ wear/src/main/java/com/dexdrip/stephenblack/nightwatch/ListenerService.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -24,13 +25,17 @@ public class ListenerService extends WearableListenerService implements GoogleAp
         GoogleApiClient.OnConnectionFailedListener {
     private static final String WEARABLE_DATA_PATH = "/nightscout_watch_data";
     private static final String WEARABLE_RESEND_PATH = "/nightscout_watch_data_resend";
+    private static final String OPEN_SETTINGS = "/openwearsettings";
     private static final String ACTION_RESEND = "com.dexdrip.stephenblack.nightwatch.RESEND_DATA";
     GoogleApiClient googleApiClient;
     private long lastRequest = 0;
 
     public class DataRequester extends AsyncTask<Void, Void, Void> {
         Context mContext;
-        DataRequester(Context context) { mContext = context; }
+
+        DataRequester(Context context) {
+            mContext = context;
+        }
 
         @Override
         protected Void doInBackground(Void... params) {
@@ -81,15 +86,24 @@ public class ListenerService extends WearableListenerService implements GoogleAp
         for (DataEvent event : dataEvents) {
 
             if (event.getType() == DataEvent.TYPE_CHANGED) {
-                String path = event.getDataItem().getUri().getPath();
-                if (path.equals(WEARABLE_DATA_PATH)) {
-                }
-                dataMap = DataMapItem.fromDataItem(event.getDataItem()).getDataMap();
 
-                Intent messageIntent = new Intent();
-                messageIntent.setAction(Intent.ACTION_SEND);
-                messageIntent.putExtra("data", dataMap.toBundle());
-                LocalBroadcastManager.getInstance(this).sendBroadcast(messageIntent);
+
+                String path = event.getDataItem().getUri().getPath();
+                if (path.equals(OPEN_SETTINGS)) {
+                    //TODO: OpenSettings
+                    Intent intent = new Intent(this, NWPreferences.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+
+                } else {
+
+                    dataMap = DataMapItem.fromDataItem(event.getDataItem()).getDataMap();
+
+                    Intent messageIntent = new Intent();
+                    messageIntent.setAction(Intent.ACTION_SEND);
+                    messageIntent.putExtra("data", dataMap.toBundle());
+                    LocalBroadcastManager.getInstance(this).sendBroadcast(messageIntent);
+                }
             }
         }
     }
@@ -114,6 +128,7 @@ public class ListenerService extends WearableListenerService implements GoogleAp
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
