f0d053f1630f09d4aa12fc1c02cc5c8df325dc73
AdrianLxM <adrian.m@inbox.com>
2015-09-21 18:30:28 +0200

    eula checkbox visible on older devices

--- mobile/src/main/java/com/dexdrip/stephenblack/nightwatch/LicenseAgreementActivity.java
+++ mobile/src/main/java/com/dexdrip/stephenblack/nightwatch/LicenseAgreementActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -12,7 +13,7 @@ import android.widget.CheckBox;
 import com.dexdrip.stephenblack.nightwatch.Activities.Home;
 
 
-public class LicenseAgreementActivity extends Activity {
+public class LicenseAgreementActivity extends ActionBarActivity {
     boolean IUnderstand;
     CheckBox agreeCheckBox;
     Button saveButton;
